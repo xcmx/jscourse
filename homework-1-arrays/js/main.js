6fecd37ca17e0cdaab60d0a86c35bb63c5a4655b
Andrew Apichenok <andrew@talentfly.com>
2023-04-15 22:20:47 +0300

    homework-1-arrays: clean up the code, add solution for negative numbers in 2 task

--- homework-1-arrays/js/main.js
+++ homework-1-arrays/js/main.js
@@ -2,19 +2,14 @@
 Задача на повернення ініціалів для кожного імені з масиву, посортованих в алфавітному порядку
 */
 const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
-let initials;
-
-initials = userNames.map(element => element.split(" ").map(element => element[0]).join('.')).sort();
-
+const initials = userNames.map(element => element.split(" ").map(element => element[0]).join('.')).sort();
 console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 
 /* Task 2 
 Задача на розворот числа
 */
-const currentMaxValue = 4589;
-let reverseMaxValue;
-
-reverseMaxValue = parseInt(currentMaxValue.toString().split('').reverse().join(''));
+const currentMaxValue = 2245;
+const reverseMaxValue = Math.sign(currentMaxValue)*parseFloat(currentMaxValue.toString().split('').reverse().join(''));
 
 console.log(reverseMaxValue); // 9854
 console.log(typeof reverseMaxValue); // 'number'
@@ -24,13 +19,9 @@ console.log(typeof reverseMaxValue); // 'number'
 Задача на знаходження добутку масиву чисел з невідомою глибиною вкладеності
 */
 const resultsArray = [1, 2, [3, [4]]];
-let productOfArray;
 
-productOfArray = 1; // For the proper multiplication
-
-productOfArray = resultsArray.flat(Infinity).reduce(
-  (accumulator, currentValue) => accumulator * currentValue,
-  productOfArray
+const productOfArray = resultsArray.flat(Infinity).reduce(
+  (accumulator, currentValue) => accumulator * currentValue, 1
 );
 
 console.log(productOfArray); // 24
