91d2328226e11fb28361757198d80decc1b320a1
Andrew Apichenok <andrew@talentfly.com>
2023-04-21 13:38:48 +0300

    homework-3-advanced-functions: add solution for the 3 task, add data validations everywhere

--- homework-3-advanced-functions/js/main.js
+++ homework-3-advanced-functions/js/main.js
@@ -6,12 +6,12 @@ console.log(addThemAll(2, 4)); // 6
 console.log(addThemAll(1,2,3,4)); // 10
 console.log(addThemAll(5,5,10)); // 20
 
-function checkIfArrayHasOnlyNumbers(number) {
+function checkIsNumber(number) {
     return typeof number === 'number';
 }
 
 function addThemAll(...numbers) {
-    if (numbers.length === 0 || !numbers.every(checkIfArrayHasOnlyNumbers)) {
+    if (numbers.length === 0 || !numbers.every(checkIsNumber)) {
         return 'Invalid data'
     }
     const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue);
@@ -20,15 +20,21 @@ function addThemAll(...numbers) {
 
 /* 2. Задача на використання замикання. Напишіть функцію яка працює таким чином: multiply(a)(b)// a * b */
 
-console.log(multiply(5)(5))	// 25
+console.log(multiply(null)(5))	// 25
 console.log(multiply(2)(-2)) // -4
 console.log(multiply(4)(3)) // 12
 
 function multiply(a) {
     if (typeof a !== 'number' || isNaN(a)) {
-        return 'Invalid data'
+        /* зробив через викидання помилки бо якщо повертати шось в ретурні, то виходить помилка
+        Uncaught TypeError: multiply(...) is not a function тому що ми ій асайнимо шось шо не схоже на функцію
+        */
+        throw new Error('First argument must be a number');
     }
     function calculate(b) {
+        if (typeof b !== 'number' || isNaN(b)) {
+            throw new Error('Second argument must be a number');
+        }
         return a * b;
     };
     return calculate;
@@ -68,16 +74,45 @@ const movies = [
     },
 ];
 
-//console.log(movies.sort(byProperty('releaseYear', '>'))); // виведе масив фільмів посортованих по року випуску, від старішого до новішого
-//console.log(movies.sort(byProperty('runningTimeInMinutes', '<'))); // виведе масив фільмів посортованих по їх тривалості, від найдовшого до найкоротшого
-//console.log(movies.sort(byProperty('movieName', '>'))); // виведе масив фільмів посортованих по назві, в алфавітному порядку
+console.log(movies.sort(byProperty('releaseYear', '>'))); // виведе масив фільмів посортованих по року випуску, від старішого до новішого
+console.log(movies.sort(byProperty('runningTimeInMinutes', '>'))); // виведе масив фільмів посортованих по їх тривалості, від найдовшого до найкоротшого
+console.log(movies.sort(byProperty('movieName', '>'))); // виведе масив фільмів посортованих по назві, в алфавітному порядку
 
 function byProperty(property, direction) {
-  if ( a.last_nom < b.last_nom ){
-    return -1;
-  }
-  if ( a.last_nom > b.last_nom ){
-    return 1;
-  }
-  return 0;
-}
+    if (direction !== '>' && direction !== '<') {
+        /* не певен що тут варто було б прям помилку видавати, 
+        бо дані все рівно віддаються, просто не посортовані як хотілося б,
+        тому не викидаю тут помилку а даю ворнінг */
+        console.warn('Wrong direction value an array wasn\'t sorted as expected');
+    }
+    return (a, b) => {
+        let valueA = a[property];
+        let valueB = b[property];
+        
+        if (typeof valueA === 'string') {
+            valueA = valueA.toLowerCase();
+        }
+        if (typeof valueB === 'string') {
+            valueB = valueB.toLowerCase();
+        }
+
+        if (direction === '>') {
+            if (valueA > valueB) {
+                return 1;
+            } else if (valueA < valueB) {
+                return -1;
+            } else {
+                return 0;
+            }
+        } else if (direction === '<') {
+            if (valueA < valueB) {
+                return 1;
+            }
+            else if (valueA > valueB) {
+                return -1;
+            } else {
+                return 0;
+            }
+        };
+    };
+};
